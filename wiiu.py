2a5e8144d2f221a67a81214c8512c2c91edcb159
thegamershollow <thegamershollow@outlook.com>
2023-05-03 17:19:56 +0000

    made changes to code

--- wiiu.py
+++ wiiu.py
@@ -1,13 +1,15 @@
-import os, pathlib, sys, urllib.request, io, requests, shutil, json, pandas as pd
+import os, sys, urllib.request, io, requests, shutil, json, pandas as pd
 from colorama import Fore; from zipfile import ZipFile; from collections import namedtuple;
 
 hbaRepo = 'https://wiiu.cdn.fortheusers.org/repo.json'
 hbaCDN = 'https://wiiu.cdn.fortheusers.org/zips/'
 hbaDL = 'https://wiiu.cdn.fortheusers.org/zips/appstore.zip'
-aromaUpdater = 'https://github.com/thegamershollow/hbl-apps/releases/download/aromaUPD/AromaUpdater.wuhb' #'https://github.com/wiiu-env/AromaUpdater/releases/'
+aromaUpdater = 'https://github.com/thegamershollow/hbl-apps/releases/download/aromaUPD/AromaUpdater.wuhb'
 aromaPackages = 'https://aroma.foryour.cafe/api/download?packages=bloopair,wiiload,ftpiiu,sdcafiine,screenshotplugin,swipswapme,environmentloader,wiiu-nanddumper-payload'
 aromaBase = 'https://github.com/wiiu-env/Aroma/releases/download/beta-14/aroma-beta-14.zip'
-oscURL = 'https://oscwii.org/library/'
+oscURL = 'https://api.oscwii.org/v2/primary/packages'
+oscCDN = 'https://hbb1.oscwii.org/hbb/'
+vwiiDl = 'https://github.com/thegamershollow/custom-tiramisu-environment/raw/main/custom-tiramisu-environment.zip'
 
 # download function with status        
 def download(url: str, fileName: str):
@@ -33,9 +35,20 @@ def download(url: str, fileName: str):
         f = open(fileName,'wb')
         f.write(r.content)
 
+# json decoder function
+def jsonDecoder(jsonDict):
+    return namedtuple('x', jsonDict.keys())(*jsonDict.values())
+
+# base homebrew check function
+def nohb():
+    noWIIU = os.path.isdir(sd+'/wiiu')
+    if noWIIU() != True:
+        print(Fore.RED+'please download the base homebrew apps before downloading anything else')
+        sys.exit(5)
+#*main function
 sdPath = ''
-path = pathlib.Path('.sdpath')
-if path.is_file() != True:
+sdVerify = os.path.isfile('.sdpath')
+if sdVerify() != True:
     giveSdPath = input('Please specify the path of your '+Fore.CYAN+'Wii U'+Fore.RESET+' SD Card: ')
     f = open('.sdpath','w')
     giveSdPath = giveSdPath.replace("'","")
@@ -45,29 +58,17 @@ if path.is_file() != True:
 f = open('.sdpath','r')
 sd = f.read()
 f.close
-sdPath = pathlib.Path(sd)
-if sdPath.exists() != True:
+sdPath = os.path.isdir(sd)
+if sdPath() != True:
     print(Fore.RED+'Please reinsert the SD Card and try again')
     sys.exit(1)
-with os.scandir(sdPath) as entries:
-    for entry in entries:
-        f = open('.cache','a')
-        f.write(entry.name)
-        f.write('\n')
-        f.close
-cache = pathlib.Path('cache')
-if cache.exists() != False:
-    shutil.rmtree('cache', ignore_errors=False, onerror=None)
-cl = pathlib.Path('.cache')
-if cl.exists() != False:
-    os.remove('.cache')
 
-prompt = input('What would you like to do?\nType the number of the corrasponding option that you want to select\n\n1. Download/Update base SD Card files\n2. Download/Update Wii U Homebrew Apps\n3. Download/Update Wii Homebrew\n4. Remove all files from Wii U SD Card\n5. Exit\n\nOption: ')
+# prompt for the whole program
+prompt = input('What would you like to do?\nType the number of the corrasponding option that you want to select\n\n1. Download/Update base SD Card files\n2. Download/Update Wii U Homebrew Apps\n3. Download/Update files needed for vWii mod\n4. Download/Update Wii Homebrew\n5. Remove all files from Wii U SD Card\n6. Use a diffrent SD card\n7. Exit\n\nOption: ')
 
 #*Download/Update Base Homebrew Files
 if prompt == '1':
     os.system('clear')
-
     # changes directory to cache
     os.chdir(sd)
     # downloads the Homebrew appstore and extracts the zip file
@@ -80,63 +81,50 @@ if prompt == '1':
     shutil.move(sd+'/manifest.install',sd+'/wiiu/apps/appstore/.get/packages/appstore')
     shutil.move(sd+'/info.json',sd+'/wiiu/apps/appstore/.get/packages/appstore')
     os.remove(sd+'/appstore.zip')
-
     # downloads the aroma packages and extracts the zip file
     aPKG = download(aromaPackages, 'aromapkgs.zip')
     aPKG = ZipFile('aromapkgs.zip','r')
     aPKG.extractall()
     aPKG.close
     os.remove(sd+'/aromapkgs.zip')
-
     # downloads the base files for aroma and extracts the zip file
     aroma = download(aromaBase, 'aroma.zip')
     aroma = ZipFile('aroma.zip','r')
     aroma.extractall()
     aroma.close
     os.remove(sd+'/aroma.zip')
-
     # copies the downloaded files from the cache directory to the sd card
     print(Fore.GREEN+'Finished downloading the '+Fore.CYAN+'base SD Card Files.'+Fore.RESET+'\n')
 
 #*Download/Update Wii U Homebrew Apps
 if prompt == '2':
     os.system('clear')
-    noWIIU = pathlib.Path(sd+'/wiiu')
-    if noWIIU.exists() != True:
-        print(Fore.RED+'please download the base homebrew apps before installing homebrew apps')
-        os.system('exit')
-        sys.exit(5)
+    nohb
     # opens the repo.json file located at: https://wiiu.cdn.fortheusers.org/repo.json
     repo = requests.get(hbaRepo)
     jsonSrc = repo.text
-    # defines a json decoder
-    def jsonDecoder(jsonDict):
-        return namedtuple('package', jsonDict.keys())(*jsonDict.values())
     pkg = json.loads(jsonSrc, object_hook=jsonDecoder)
     count = 0
     pkgTotal = pkg.packages.__len__()
-
     # create an empty list
     allPkg = []
-
     # iterate through items in json file
     for items in pkg.packages.__iter__():
         allPkg.append(pkg.packages[count])
         count = count+1
     allPkg = sorted(allPkg)
-    # converts list into a printable table
-    table = pd.DataFrame(allPkg)
-    table.drop(columns=["binary", "title", "license", "url", "changelog", "screens", "extracted", "details", "md5", "description"],inplace=True,)
-    table = table.reindex(columns=["name", "author", "category", "version", "filesize"])
-    table.rename(columns={"category": "Category", "version" : "Version", "filesize" : "Download Size(KB)", "app_dls" : "App Downloads", "author" : "Author", "updated" : "Update Date","name" : "App Name"},inplace=True,)
-    print(table.to_string())
-
+    # converts list into a printable pkgTable
+    pkgTable = pd.DataFrame(allPkg)
+    pkgTable.drop(columns=["binary", "title", "license", "url", "changelog", "screens", "extracted", "details", "md5", "description"],inplace=True,)
+    pkgTable = pkgTable.reindex(columns=["name", "author", "category", "version", "filesize"])
+    pkgTable.rename(columns={"category": "Category", "version" : "Version", "filesize" : "Download Size(KB)", "app_dls" : "App Downloads", "author" : "Author", "updated" : "Update Date","name" : "App Name"},inplace=True,)
+    print(pkgTable.to_string())
     # asks for input of what app/s you want to download
     hbSelect = input('Type the app/s name/ to download it '+Fore.LIGHTCYAN_EX+'**if multiple are selected this process will take a lot longer**'+Fore.RESET+'\n\nSeperate the app names with commas if you want to download multiple apps at once.\n\nSelection: '); hbSelect = hbSelect.split(',')
-    apps = table['App Name'].values.tolist()
+    apps = pkgTable['App Name'].values.tolist()
     os.chdir(sd)
-    pkgPath = pathlib.Path(sd+'/wiiu/apps/appstore/.get/packages')
-    if pkgPath.exists() != True:
+    pkgPath = os.path.isdir(sd+'/wiiu/apps/appstore/.get/packages')
+    if pkgPath() != True:
         os.mkdir(sd+'/wiiu/apps/appstore/.get/packages')
     # downloads the homebrew apps specified in hbSelect
     for item in hbSelect:
@@ -147,23 +135,71 @@ if prompt == '2':
             dl.extractall()
             dl.close
             print('Copied '+Fore.CYAN+item+Fore.RESET+' to the SD card\n')
-            dlPath = pathlib.Path(sd+'/wiiu/apps/appstore/.get/packages/'+item)
-            if dlPath.exists() != False:
+            dlPath = os.path.isdir(sd+'/wiiu/apps/appstore/.get/packages/'+item)
+            if dlPath() != False:
                 os.remove(sd+'/manifest.install')
                 os.remove(sd+'/info.json')
-            if dlPath.exists() != True:
+            if dlPath() != True:
                 os.mkdir(sd+'/wiiu/apps/appstore/.get/packages/'+item)
                 shutil.move(sd+'/manifest.install',sd+'/wiiu/apps/appstore/.get/packages/'+item)
                 shutil.move(sd+'/info.json',sd+'/wiiu/apps/appstore/.get/packages/'+item)
             os.remove(sd+'/'+item+'.zip')
     print('\n'+Fore.GREEN+'Finished downloading app/s')
 
-#*Download/Update VWii Homebrew Apps
+#*Download/Update vWii mod files
 if prompt == '3':
+    nohb
     os.system('clear')
+    os.chdir(sd)
+    vWii = download(vwiiDl,'vwii.zip')
+    os.remove(sd+'/vwii.zip')
 
-#* Delete all files from SD card
+#*Download/Update VWii Homebrew Apps
 if prompt == '4':
+    os.system('clear')
+    nohb
+    oscApi = requests.get(oscURL)
+    jsonFRMT = '{\n     "packages":'
+    after = '}'
+    jsonSrc = f'{jsonFRMT}{oscApi.text}'
+    jsonSrc = jsonSrc.replace(']',' ]')
+    jsonSrc += after
+    osc = json.loads(jsonSrc, object_hook=jsonDecoder)
+    count = 0
+    oscTotal = osc.packages.__len__()
+    allOSC = []
+    for items in osc.packages.__iter__():
+        allOSC.append(osc.packages[count])
+        count = count+1
+    allOSC = sorted(allOSC)
+    oscTable = pd.DataFrame(allOSC)
+    apps = oscTable['internal_name'].values.tolist()
+    oscTable.drop(columns=["downloads", "extra_directories", "icon_url", "rating", "release_date", "shop_title_id", "shop_title_version", "long_description", "updated", "zip_size", "display_name", "package_type"],inplace=True,)
+    oscTable = oscTable.reindex(columns=["internal_name", "category", "version", "short_description", "coder"])
+    oscTable.rename(columns={"internal_name":"App Name","category":"Catergory","version":"Version","short_description":"Decription","coder":"Author"},inplace=True,)
+    f = open('text.txt','w')
+    f.write(oscTable.to_string())
+    print(oscTable.to_string())
+    oscSelect = input('Type the app/s name/ to download it '+Fore.LIGHTCYAN_EX+'**if multiple are selected this process will take a lot longer**'+Fore.RESET+'\n\nSeperate the app names with commas if you want to download multiple apps at once.\n\nSelection: '); oscSelect = oscSelect.split(',')
+    os.chdir(sd)
+    oscPath = os.path.isdir(sd+'/apps')
+    if oscPath() != True:
+        os.mkdir(sd+'/apps')
+    for item in oscSelect:
+        if item in apps:
+            dlURL = oscCDN+item+'/'+item+'.zip'
+            dl = download(dlURL,item+'.zip')
+            dl = ZipFile(item+'.zip')
+            dl.extractall()
+            dl.close
+            print('Copied '+Fore.CYAN+item+Fore.RESET+' to the SD card\n')
+            #dlPath = os.path(sd+'/wiiu/apps/appstore/.get/packages/'+item)
+            os.remove(sd+'/'+item+'.zip')
+    print('\n'+Fore.GREEN+'Finished downloading app/s')
+    
+
+#*Delete all files from SD card
+if prompt == '5':
     os.system('clear')
     warn=input(Fore.RED+'*⚠️WARNING⚠️* '+Fore.RESET+'This will delete/remove all files from the SD card\nContinue (Y/N):\n')
     if warn == 'Y' or warn == 'y':
@@ -171,7 +207,13 @@ if prompt == '4':
         os.system('exit')
         sys.exit()
     sys.exit()
+
+#*Use a different sd card
+if prompt == '6':
+    os.system('clear')
+    os.remove('.sdpath')
+
 #*Exit program
-if prompt == '5':
+if prompt == '7':
     os.system('exit')
     sys.exit()
